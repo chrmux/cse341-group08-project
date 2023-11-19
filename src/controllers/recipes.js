22e695bfd6018b89908642b14826e2bdf7f0a945
gavin-cannon <gavincannon@byui.edu>
2023-11-18 23:43:59 -0700

    Added comments to controllers, and added in validate helper and started validation file

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -1,20 +1,25 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
-const getRecipes = async(req, res, next) => {
-    console.log("In get Recipes");
-    try {
-        const result = await mongodb.getDb().db('Group08-Project03').collection('Recipes').find();
-        result.toArray().then((lists) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists);
-        });
-    } catch (e) {
-        console.log(e);
-        res.status(400).json("Error in Get Recipes");
-    }
+// Get all recipes
+const getRecipes = async (req, res, next) => {
+  console.log("In get Recipes");
+  try {
+    const result = await mongodb
+      .getDb()
+      .db("Group08-Project03")
+      .collection("Recipes")
+      .find();
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json("Error in Get Recipes");
+  }
 };
 
 module.exports = {
-    getRecipes
+  getRecipes,
 };
